943977dac01c5d28e35a98fecd96963ed7e922e4
Arshit rupani <125478383+arshitrupanii@users.noreply.github.com>
2025-05-15 19:21:54 +0530

    up

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from '@/components/theme-provider'
 import Navbar from '@/components/navbar'
-// import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ['latin'] })
 
